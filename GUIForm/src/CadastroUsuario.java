ac1ef5d61d5b858d135548d23ab4ddcc8a7a4959
jocxzs <joqfra07@gmail.com>
2025-08-10 11:42:26 -0300

    campos vazios e placeholder

--- GUIForm/src/CadastroUsuario.java
+++ GUIForm/src/CadastroUsuario.java
@@ -6,11 +6,11 @@ import java.awt.event.FocusEvent;
 
 public class CadastroUsuario extends JFrame {
     private JLabel jlEmail;
-    private JTextField jtfEmail;
+    private JTextField tfEmail;
     private JLabel jlSenha;
-    private JPasswordField passwordSenha;
     private JButton cadastrarButton;
     private JPanel MainPanel;
+    private JTextField tfSenha;
 
     public CadastroUsuario() { //cria a interface e suas propriedades]
             setContentPane(MainPanel);
@@ -22,23 +22,53 @@ public class CadastroUsuario extends JFrame {
         cadastrarButton.addActionListener(new ActionListener() { //escutador de ação para o botao funcionar
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(CadastroUsuario.this,"Usuário cadastrado");//pop-up de usuario cadastrado
+                if (tfSenha.getText().equals("") || //tratamento de campo vazio
+                        tfEmail.getText().equals("") ||
+                        tfSenha.getText().equals("Your Password") ||
+                        tfEmail.getText().equals("example@yourdomain.com")) {
+                    JOptionPane.showMessageDialog(CadastroUsuario.this, "Os campos não podem estar vazios");//pop-up de campos vazios
+                }else{
+                    JOptionPane.showMessageDialog(CadastroUsuario.this,"Usuário cadastrado");//pop-up de usuario cadastrado
+                }
             }
         });
-        jtfEmail.addFocusListener(new FocusAdapter() {//faz o texto do email sumir quando ganhar foco
+        tfEmail.addFocusListener(new FocusAdapter() {//faz o texto do campo email sumir quando ganhar foco
             @Override
             public void focusGained(FocusEvent e) {
-                super.focusGained(e);
-                jtfEmail.setText("");
+                if(tfEmail.getText().equals("example@yourdomain.com")) {// faz com que oq for digitado pelo usuario nao seja apagado
+                    super.focusGained(e);
+                    tfEmail.setText("");
+                }
             }
         });
-        jtfEmail.addFocusListener(new FocusAdapter() {
+        tfEmail.addFocusListener(new FocusAdapter() {//faz o texto do campo email voltar quando perder foco
             @Override
             public void focusLost(FocusEvent e) {
-                super.focusLost(e);
-                jtfEmail.setText("example@yourdomain.com");
+                if(tfEmail.getText().equals("")) {// faz com que oq for digitado pelo usuario nao seja apagado
+                    super.focusLost(e);
+                    tfEmail.setText("example@yourdomain.com");
+                }
             }
         });
+        tfSenha.addFocusListener(new FocusAdapter() {//faz o texto do campo senha sumir quando ganhar foco
+            @Override
+            public void focusGained(FocusEvent e) {
+                if(tfSenha.getText().equals("Your Password")) {// faz com que oq for digitado pelo usuario nao seja apagado
+                    super.focusGained(e);
+                    tfSenha.setText("");
+                }
+            }
+        });
+        tfSenha.addFocusListener(new FocusAdapter() {//faz o texto do campo senha voltar quando perder foco
+            @Override
+            public void focusLost(FocusEvent e) {
+                if(tfSenha.getText().equals("")) {// faz com que oq for digitado pelo usuario nao seja apagado
+                    super.focusLost(e);
+                    tfSenha.setText("Your Password");
+                }
+            }
+        });
+
     }
         public static void main (String[] args) { //roda o app
             new CadastroUsuario();
